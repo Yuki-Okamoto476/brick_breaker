3fa810f2e5abcea3deb0a94bf60eca46e9669d14
Yuki-Okamoto476 <okayu110108@gmail.com>
2021-09-26 18:08:19 +0900

    Implement the collision detection and paddle

--- script.js
+++ script.js
@@ -1,3 +1,75 @@
 const canvas = document.getElementById('myCanvas')
 const ctx = canvas.getContext("2d")
-console.log(ctx);
+
+let x = canvas.width / 2
+let y = canvas.height - 30
+let dx = 2
+let dy = -2
+const ballRadius = 5
+const paddleHeight = 10
+const paddleWidth = 75
+let paddleX = (canvas.width - paddleWidth) / 2
+let rightPressed = false
+let leftPressed = false
+
+const keyDownHandler = (e) => {
+  if (e.key === "Right" || e.key === "ArrowRight") {
+    rightPressed = true 
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    leftPressed = true
+  }
+}
+const keyUpHandler = (e) => {
+  if (e.key === "Right" || e.key === "ArrowRight") {
+    rightPressed = false
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    leftPressed = false
+  }
+}
+
+document.addEventListener("keydown", keyDownHandler, false)
+document.addEventListener("keyup", keyUpHandler, false)
+
+const drawBall = () => {
+  ctx.beginPath()
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2)
+  ctx.fillStyle = "#0095DD"
+  ctx.fill()
+  ctx.closePath()
+}
+
+const drawPaddle = () => {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+const draw = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  drawBall()
+  drawPaddle()
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx
+  }
+  if (y + dy < ballRadius) {
+    dy = -dy
+  } else if (y + dy > canvas.height - ballRadius) {
+    if (x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy
+    } else {
+      alert("Game Over")
+      document.location.reload()
+      clearInterval(interval)
+    }
+  }
+  if (rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 7
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 7
+  }
+  x += dx;
+  y += dy;
+}
+const interval = setInterval(draw, 10)
