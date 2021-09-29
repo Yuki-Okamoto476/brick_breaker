881a6eb2183673e3e8369b2af7f204b299a63a50
Yuki-Okamoto476 <okayu110108@gmail.com>
2021-09-30 08:34:32 +0900

    Completed

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById('myCanvas')
 const ctx = canvas.getContext("2d")
 
 const startButton = document.querySelector('.btn-engine-start-in')
+const scoreView = document.querySelector('.scoreView')
 
 let x = canvas.width / 2 //ボールの開始位置のx座標（canvasの左端からボールまでの距離）
 let y = canvas.height - 30 //ボールの開始位置のy座標（canvasの下端から30px上）
@@ -22,12 +23,12 @@ const brickOffsetTop = 20 //canvasの上端からブロックまでの間隔
 const brickOffsetLeft = 5　//canvasの左端からブロックまでの間隔
 let score = 0
 
-//5×3=15のブロックを作成
+//座標(0,0)に5×3=15個のブロックを作成
 const bricks = []
 for (let i = 0; i < brickColumnCount; i++) {
   bricks[i] = []
   for (let j = 0; j < brickRowCount; j++) {
-    bricks[i][j] = { x: 0, y: 0, status: 1 }
+    bricks[i][j] = { x: 0, y: 0, status: 1 } //衝突検知のフラグ（ボールがぶつかっていないときはstatus=1, ボールがぶつかったらstatus=0）
   }
 }
 //キーボードの←または→ボタンが押されたときにフラグをtrueにする処理
@@ -71,39 +72,24 @@ const drawBall = () => {
 //パドルを描画する処理
 const drawPaddle = () => {
   ctx.beginPath();
-  ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-  ctx.fillStyle = "yellow";
+  ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight) //rect(四角形の左上のx座標, 四角形の左上のy座標, 四角形の幅, 四角形の高さ)
+  ctx.fillStyle = "yellow"; //パドルの色
   ctx.fill();
   ctx.closePath();
 }
 
-const drawBricks = () => {
-  for (let i = 0; i < brickColumnCount; i++) {
-    for (let j = 0; j < brickRowCount; j++) {
-      if (bricks[i][j].status == 1) {
-        let brickX = (i*(brickWidth + brickPadding)) + brickOffsetLeft
-        let brickY = (j*(brickHeight + brickPadding)) + brickOffsetTop
-        bricks[i][j].x = brickX
-        bricks[i][j].y = brickY
-        ctx.beginPath()
-        ctx.rect(brickX, brickY, brickWidth, brickHeight)
-        ctx.fillStyle = "#0095DD"
-        ctx.fill()
-        ctx.closePath()
-      }
-  }
-}
-}
-
+//衝突を検知する処理
 const collisionDetection = () => {
   for (let i = 0; i < brickColumnCount; i++) {
     for (let j = 0; j < brickRowCount; j++) {
       let b = bricks[i][j]
       if (b.status == 1) {
+        //ボールのx座標がブロックのx座標より大きい、かつボールのx座標がブロックのx座標とその幅の和より小さい、かつボールのy座標がブロックのy座標より大きい、かつボールのy座標がブロックのy座標とその高さの和より小さいのときの処理
         if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
-          dy = -dy
-          b.status = 0
-          score++
+          dy = -dy //ボールの跳ね返り
+          b.status = 0 //ブロックを削除
+          score++ 
+          //すべてのブロックを消したらゲームクリア
           if (score == brickRowCount * brickColumnCount) {
             alert('クリア')
             location.reload()
@@ -114,35 +100,55 @@ const collisionDetection = () => {
   }
 }
 
-const drawScore = () => {
-  ctx.font = "14px"
-  ctx.fillStyle = "#0095DD"
-  ctx.fillText("Score: " + score, 8, 10)
+//ブロックを描画する処理
+const drawBricks = () => {
+  for (let i = 0; i < brickColumnCount; i++) {
+    for (let j = 0; j < brickRowCount; j++) {
+      //status=1のときにブロック描画
+      if (bricks[i][j].status == 1) {
+        let brickX = (i*(brickWidth + brickPadding)) + brickOffsetLeft //ブロックのx座標
+        let brickY = (j*(brickHeight + brickPadding)) + brickOffsetTop //ブロックのy座標
+        bricks[i][j].x = brickX
+        bricks[i][j].y = brickY
+        ctx.beginPath()
+        ctx.rect(brickX, brickY, brickWidth, brickHeight)
+        ctx.fillStyle = "white"
+        ctx.fill()
+        ctx.closePath()
+      }
+  }
+}
 }
 
 const draw = () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.clearRect(0, 0, canvas.width, canvas.height) //ボールの軌跡を削除
   drawBricks()
   drawBall()
   drawPaddle()
-  drawScore()
   collisionDetection()
+  scoreView.innerHTML = score //スコアを表示
+  //ボールが左端または右端に衝突したときに跳ね返る処理
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx
   }
+  //ボールが上端に衝突した時に跳ね返る処理
   if (y + dy < ballRadius) {
     dy = -dy
+    //ボールが下端に衝突した時に跳ね返る処理
   } else if (y + dy > canvas.height - ballRadius) {
+    //ボールがパドルに当たったときに跳ね返る処理
     if (x > paddleX && x < paddleX + paddleWidth) {
       dy = -dy
     } else {
       alert("Game Over")
-      document.location.reload()
+      document.location.reload() //ゲームオーバーになったら画面をリロードして最初の画面に戻す
       clearInterval(interval)
     }
   }
+  //→ボタンが押されたときにパドルを右に7動かす処理
   if (rightPressed && paddleX < canvas.width - paddleWidth) {
     paddleX += 7
+    //←ボタンが押されたときにパドルを左に7動かす処理
   } else if (leftPressed && paddleX > 0) {
     paddleX -= 7
   }
@@ -151,6 +157,7 @@ const draw = () => {
   requestAnimationFrame(draw);
 }
 
+//スタートボタンを押してゲームを開始する処理
 startButton.addEventListener('click', () => {
   draw()
 })
